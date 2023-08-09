56ab7397a3c5946c652967f506fe25d9a79e9cea
buituantai2k4 <taibuiz2004@gmai.com>
2023-08-09 14:37:07 +0700

    #hello

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 require('dotenv').config()  
-
+const dbConnect = require('./config/dbconnect')
 
 
 const app = express()
@@ -9,6 +9,7 @@ app.use(express.json())
 app.use(express.urlencoded({
     extended: true
 }))
+dbConnect()
 
 app.use('/', (req, res) => {
     res.send('SEVER ON')
